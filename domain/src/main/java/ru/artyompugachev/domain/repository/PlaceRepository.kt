0e5c009dee7fb796014a5f7b4d256f20189633e8
Artyom Pugachev <artempugachev1@gmail.com>
2018-07-25 20:14:17 +0300

    remove place repository for now

--- domain/src/main/java/ru/artyompugachev/domain/repository/PlaceRepository.kt
+++
@@ -1,9 +0,0 @@
-package ru.artyompugachev.domain.repository
-
-import io.reactivex.Observable
-import ru.artyompugachev.domain.model.Place
-
-interface PlaceRepository {
-
-    fun getPlace(): Observable<Place>
-}
