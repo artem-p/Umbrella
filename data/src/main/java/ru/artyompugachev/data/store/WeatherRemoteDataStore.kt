bb46ca6f8abdc3d7bc8979888c652322c14ce353
Artyom Pugachev <artempugachev1@gmail.com>
2018-07-26 20:45:02 +0300

    add weather remote data store

---
+++ data/src/main/java/ru/artyompugachev/data/store/WeatherRemoteDataStore.kt
@@ -0,0 +1,27 @@
+package ru.artyompugachev.data.store
+
+import io.reactivex.Completable
+import io.reactivex.Observable
+import ru.artyompugachev.data.model.WeatherEntity
+import ru.artyompugachev.data.repository.WeatherDataStore
+import ru.artyompugachev.data.repository.WeatherRemote
+import javax.inject.Inject
+
+class WeatherRemoteDataStore @Inject constructor(private val weatherRemote: WeatherRemote):
+        WeatherDataStore {
+    override fun getCurrentWeather(): Observable<WeatherEntity> {
+        return weatherRemote.getCurrentWeather()
+    }
+
+    override fun getForecasts(): Observable<List<WeatherEntity>> {
+        return weatherRemote.getForecasts()
+    }
+
+    override fun saveWeather(currentWeather: WeatherEntity, forecasts: List<WeatherEntity>): Completable {
+        throw UnsupportedOperationException("Saving weather isn't supported in the remote data store...")
+    }
+
+    override fun clearWeather(): Completable {
+        throw UnsupportedOperationException("Clearing weather isn't supported in the remote data store...")
+    }
+}
