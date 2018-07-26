625f7167a021bea482edbfc1fc2a71b9a4d9b7c4
Artyom Pugachev <artempugachev1@gmail.com>
2018-07-26 22:50:24 +0300

    implement weather data repository

--- data/src/main/java/ru/artyompugachev/data/WeatherDataRepository.kt
+++ data/src/main/java/ru/artyompugachev/data/WeatherDataRepository.kt
@@ -2,28 +2,29 @@ package ru.artyompugachev.data
 
 import io.reactivex.Observable
 import io.reactivex.functions.BiFunction
-import ru.artyompugachev.data.mapper.WeatherRecordMapper
+import ru.artyompugachev.data.mapper.WeatherMapper
 import ru.artyompugachev.data.repository.WeatherCache
 import ru.artyompugachev.data.store.WeatherDataStoreFactory
+import ru.artyompugachev.domain.model.Weather
 import ru.artyompugachev.domain.model.WeatherRecord
 import ru.artyompugachev.domain.repository.WeatherRepository
 import javax.inject.Inject
 
 class WeatherDataRepository @Inject constructor(
-        private val mapper: WeatherRecordMapper,
+        private val mapper: WeatherMapper,
         private val cache: WeatherCache,
         private val factory: WeatherDataStoreFactory
 ): WeatherRepository {
 
-    override fun getCurrentWeather(): Observable<WeatherRecord> {
+    override fun getWeather(): Observable<Weather> {
 
         return Observable.zip(cache.isWeatherCached().toObservable(),
-                cache.isWeatherCacheExpired().toObservable(),
+                cache.isExpired().toObservable(),
                 BiFunction<Boolean, Boolean, Pair<Boolean, Boolean>> { areCached, isExpired ->
                     Pair(areCached, isExpired)
                 })
                 .flatMap {
-                    factory.getDataStore(it.first, it.second).getCurrentWeather()
+                    factory.getDataStore(it.first, it.second).getWeather()
                 }
 
                 .flatMap { weather ->
@@ -31,14 +32,6 @@ class WeatherDataRepository @Inject constructor(
                             .saveWeather(weather)
                             .andThen(Observable.just(weather))
                 }
-                .map {
-                    it.map {
-                        mapper.mapFromEntity(it)
-                    }
-                }
-    }
-
-    override fun getForecasts(): Observable<List<WeatherRecord>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                .map { mapper.mapFromEntity(it) }
     }
 }
