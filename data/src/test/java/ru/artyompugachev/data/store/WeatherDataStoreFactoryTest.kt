563687a7db4c1238020b98b60a77d642282db9f8
Artyom Pugachev <artempugachev1@gmail.com>
2018-07-29 19:26:02 +0300

    add weather data store factory test

---
+++ data/src/test/java/ru/artyompugachev/data/store/WeatherDataStoreFactoryTest.kt
@@ -0,0 +1,36 @@
+package ru.artyompugachev.data.store
+
+import com.nhaarman.mockito_kotlin.mock
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class WeatherDataStoreFactoryTest {
+    private val cacheStore = mock<WeatherCacheDataStore>()
+    private val remoteStore = mock<WeatherRemoteDataStore>()
+    private val factory = WeatherDataStoreFactory(cacheStore, remoteStore)
+
+
+
+    @Test
+    fun getDataStoreReturnsCacheStoreWhenCacheExists() {
+        assertEquals(cacheStore, factory.getDataStore(true, false))
+    }
+
+
+    @Test
+    fun getDataStoreReturnsRemoteStoreWhenCacheExpired() {
+        assertEquals(remoteStore, factory.getDataStore(true, true))
+    }
+
+
+    @Test
+    fun getDataStoreReturnsRemoteStoreWhenWeatherNotCached() {
+        assertEquals(remoteStore, factory.getDataStore(false, false))
+    }
+
+
+    @Test
+    fun getCacheDataStoreReturnsCacheStore() {
+        assertEquals(cacheStore, factory.getCacheDataStore())
+    }
+}
